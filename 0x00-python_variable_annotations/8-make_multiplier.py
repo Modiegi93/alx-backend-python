da1cbf0bcc67b1c05d03aacb2bc3e55cca1a2b0e
Mathapelo <modiegi.mphahlele93@gmail.com>
2023-07-06 11:20:19 +0000

    fixing codestyle

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -6,6 +6,10 @@ from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+        takes a float multiplier as argument and returns a function
+        that multiplies a float by multiplier
+    """
     def multiply(value: float) -> float:
         return value * multiplier
     return multiply
--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -6,4 +6,11 @@ from typing import Iterable, Sequence, List, Tuple
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+        Args:
+            lst: Sequence of list
+
+        Return:
+            List of tuple of sequence of integers
+    """
     return [(i, len(i)) for i in lst]
