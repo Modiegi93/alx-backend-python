1c013bfa4cbfee41c5665c4019e0ca4cdc465190
Mathapelo <modiegi.mphahlele93@gmail.com>
2023-07-06 11:30:38 +0000

    fixing codestyle

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -5,4 +5,8 @@ A type-annotated function add
 
 
 def add(a: float, b: float) -> float:
+    """
+        takes a float a and a float b as arguments
+        and returns their sum as a float.
+    """
     return a + b
--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -5,4 +5,8 @@ A type-annotated function concat
 
 
 def concat(str1: str, str2: str) -> str:
+    """
+        takes a string str1 and a string str2 as arguments
+        and returns a concatenated string
+    """
     return str1 + str2
