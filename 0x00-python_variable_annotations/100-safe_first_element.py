22f35e22790703daa4a03b2bf7861073f29a50bc
Mathapelo <modiegi.mphahlele93@gmail.com>
2023-07-06 11:14:28 +0000

    fixing codestyle

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -6,6 +6,13 @@ from typing import Sequence, Any, Union, Optional
 
 
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, Optional[None]]:
+    """
+        Args:
+            lst: Any data type
+
+        Return:
+            None or first element
+    """
     if lst:
         return lst[0]
     else:
