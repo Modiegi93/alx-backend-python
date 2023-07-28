2e1df869648db137cfeb6915989d55a35c4014d3
Mathapelo <modiegi.mphahlele93@gmail.com>
2023-07-28 00:47:59 -0700

    task 4 - 7

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+"""GithubOrgClient"""
+import unittest
+import json
+from unittest.mock import patch, PropertyMock, Mock
+from parameterized import parameterized
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """ Class to test GithubOrgClient"""
+    
+    @parameterized.expand([
+        ("google"),
+        ("abc")
+    ])
+    @patch('client.get_json')
+    def test_org(self, input, mock):
+        """method to test that GithubOrgClient returns the correct value"""
+        test_class = GithubOrgClient(input)
+        test_class.org()
+        mock.assert_called_once_with(f"https://api.github.com/orgs/{input}")
+
+    def test_public_repos_url(self):
+        """ Test that the result of _public_repos_url is the expected
+            one based on the mocked payload.
+        """
+        with patch('client.GithubOrgClient.org',
+                new_callable=PropertyMock) as mock:
+            payload = {"repos_url": "World"}
+            mock.return_value = payload
+            test_class = GithubOrgClient('test')
+            result = test_class._public_repos_url
+            self.assertEqual(result, payload["repos_url"])
+
+    @patch('client.get_json')
+    def test_public_repos(self, mock_json):
+        """ Test that the list of repos is what you expect from the chosen payload.
+            Test that the mocked property and the mocked get_json was called once.
+        """
+        json_payload = [{"name": "Google"}, {"name": "Twitter"}]
+        mock_json.return_value = json_payload
+
+        with patch('client.GithubOrgClient._public_repos_url',
+                new_callable=PropertyMock) as mock_public:
+
+            mock_public.return_value = "hello/world"
+            test_class = GithubOrgClient('test')
+            result = test_class.public_repos()
+
+            check = [i["name"] for i in json_payload]
+            self.assertEqual(result, check)
+
+            mock_public.assert_called_once()
+            mock_json.assert_called_once()
+
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """ Test for GithubOrgClient.has_license"""
+        result = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(result, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
